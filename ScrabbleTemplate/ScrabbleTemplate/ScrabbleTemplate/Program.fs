3adab5864d456f509a10f75ddd45d3c488ab183a
Maxime Birkkjær Havez <maxih@hotmail.dk>
2023-04-12 11:40:33 +0200

    Game starts with our bot

--- ScrabbleTemplate/ScrabbleTemplate/ScrabbleTemplate/Program.fs
+++ ScrabbleTemplate/ScrabbleTemplate/ScrabbleTemplate/Program.fs
@@ -53,7 +53,7 @@ let main argv =
     let (dictionary, time) =
         time (fun () -> ScrabbleUtil.Dictionary.mkDict words dictAPI)
 
-    let players = spawnMultiples "OxyphenButazone" dictionary Oxyphenbutazone.Scrabble.startGame 2
+    let players = spawnMultiples "ourBot" dictionary YourClientName.Scrabble.startGame 1
 
 
     do ScrabbleServer.Comm.startGame 
